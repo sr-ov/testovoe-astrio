3c6b10720e5ef47d9dff5533398b41842cd565d9
sr-ov <c8@yandex.ru>
2022-06-09 19:34:12 +0400

    изм app-product-conf

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import Toast, { POSITION } from 'vue-toastification'
-import 'vue-toastification/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
@@ -9,9 +7,6 @@ import './index.css'
 
 const app = createApp(App)
 
-app.use(Toast, {
-	position: POSITION.TOP_CENTER,
-})
 app.use(createPinia())
 app.use(router)
 
